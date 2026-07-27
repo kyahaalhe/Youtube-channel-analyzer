e9034de83a01d989ad70019837dfcf1226e6ec91
kyahaalhe <Kiyahaalhebhai@gmail.com>
2026-07-27 05:43:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://youtube-channel-analyzer-y.vercel.app/";
+const BACKEND_URL = "https://youtube-channel-analyzer-76.up.railway.app";
 
 let currentVideoCount = 8;
 let currentChannelId = "";
